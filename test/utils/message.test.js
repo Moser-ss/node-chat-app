05ac21e064f916dc23c8ccf847e96d3ef759ebd3
Stephane Moser <moser.sts@gmail.com>
2018-12-23 00:19:33 +0000

    Add tests for geolocation

--- test/utils/message.test.js
+++ test/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const { generateMessage }= require('../../server/utils/message.js');
+const { generateMessage , generateLocationMessage}= require('../../server/utils/message.js');
 
 describe('generateMessage', () => {
     it('should generate the correct message object', () => {
@@ -11,5 +11,21 @@ describe('generateMessage', () => {
             text
         })
         expect(message.createAt).to.be.an('number')
+    })
+})
+
+describe('generateLocationMessage', () => {
+    it('should generate the correct location object', () => {
+        const googleEndpoint = 'https://www.google.com/maps?q='
+        const latitude = -48.53997
+        const longitude = -63.60848
+        const from = 'system'
+        const message = generateLocationMessage(from,latitude, longitude)
+        expect(message.from).to.be.equal(from)
+        expect(message.createAt).to.be.an('number')
+        expect(message.url).to.include(latitude)
+        expect(message.url).to.include(longitude)
+        expect(message.url).to.include(googleEndpoint)
+
     })
 })
