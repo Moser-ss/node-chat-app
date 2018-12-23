8f3ecbe9006c543ee40e8dad3fa0082f7a4d6a47
Stephane Moser <moser.sts@gmail.com>
2018-12-23 23:16:40 +0000

    Remove uneed code

--- server/server.js
+++ server/server.js
@@ -12,21 +12,6 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT
 
 app.use(express.static(publicPath));
-app.use((req, res, next) => {
-    let now = new Date().toString();
-    let log = `${now}: ${req.method} ${req.url}`
-    console.log(log);
-    fs.appendFile('server.log', log + '\n', (err) => {
-        if (err) {
-            console.log('Unable to apped to server.log');
-        }
-    })
-    next();
-})
-
-app.get('/', (request, response) => {
-    response.render(publicPath)
-})
 
 io.on('connection', (socket) => {
     console.log('New user connected');
