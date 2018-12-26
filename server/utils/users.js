f9d68c9bbac6e2770a3a5f198848c72120559029
Stephane Moser <moser.sts@gmail.com>
2018-12-26 10:44:53 +0000

    Add User Class and tests

---
+++ server/utils/users.js
@@ -0,0 +1,42 @@
+class Users {
+    constructor () {
+        this.users = [];
+    }
+
+    addUser(id, name , room) {
+        const user = { id, name, room };
+        this.users.push(user);
+        return user;
+    }
+
+    removeUser (id) {
+        let removedUser;
+        this.users = this.users.filter((user) =>{
+            if (user.id !== id) {
+                return true;
+            }
+            else {
+                removedUser = user;
+                return false;
+            }
+        } );
+        return removedUser;
+    }
+
+    getUser (id){
+        return this.users.find( (user) => user.id === id);
+    }
+
+    getUserList (room){
+        return this.users.filter((user) => user.room === room );
+    }
+
+    getUserNameList (room) {
+        return this.getUserList(room).map((user) => user.name);
+    }
+
+}
+
+module.exports = {
+    Users
+};
---
+++ test/utils/users.test.js
@@ -0,0 +1,70 @@
+const { expect } = require('chai');
+const { Users } = require('../../server/utils/users');
+
+describe('Users', () => {
+    let users ;
+
+    beforeEach(() => {
+        users = new Users();
+        users.users = require('./_data/users.json');    
+    });
+
+    it('should add a new user', () => {
+        users = new Users();
+        const user = {
+            id: '4bnGtHKVDx',
+            name: 'Alice',
+            room: 'earth'
+        };
+
+        const resUser = users.addUser(user.id,user.name , user.room);
+        expect(users.users).to.deep.equal([resUser]);
+    });
+
+    it('should remove a user', () => {
+        const userToRemove = users.users[0];
+        const intialUserNumber = users.users.length;
+        const userRemoved = users.removeUser(userToRemove.id);
+        expect(userRemoved).to.be.deep.equals(userToRemove);
+        expect(users.users.length).to.be.equal(intialUserNumber -1 );
+        expect(users.users).to.not.include(userToRemove);
+    });
+
+    it('should not remove user', () => {
+        const intialUserNumber = users.users.length;
+        const userRemoved = users.removeUser('notValid');
+        expect(userRemoved).to.be.an('undefined');
+        expect(users.users.length).to.be.equal(intialUserNumber);
+    });
+    
+    it('should find user', () => {
+        const userToFind = users.users[0];
+        const user = users.getUser(userToFind.id);
+        expect(user).to.be.deep.equal(user);
+    });
+
+    it('should not find user', () => {
+        const user = users.getUser('notValid');
+        expect(user).to.be.an('undefined');
+    });
+
+    it('should return a list of users in room invention', () => {
+        const usersList = users.getUserList('invention');
+        expect(usersList).to.be.deep.equal([users.users[0], users.users[2]]);
+    });
+
+    it('should return list of names in the room invention', () => {
+        const usersList = users.getUserNameList('invention');
+        expect(usersList).to.be.deep.equal(['Alice','Charlie']);
+    });
+
+    it('should return a empty list of users', () => {
+        const usersList = users.getUserList('banana');
+        expect(usersList).to.be.deep.equals([]);
+    });
+
+    it('should return a empty list of user names', () => {
+        const usersList = users.getUserNameList('ananas');
+        expect(usersList).to.be.deep.equals([]);
+    });
+});
